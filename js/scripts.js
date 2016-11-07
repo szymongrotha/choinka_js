d82149a6886c51405701d6a1609bb1d4d4d00dd9
Szymon <szymon.grotha@gmail.com>
2016-11-07 13:09:18 +0100

    Firt version of Christmas Tree JS funcion

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+	function rysujChoinke(x) {
+		for (var i = 0; i < x; i++) {
+			var star = " ";
+				for (var j = 0 ; j < i * 2 - 1 ; j++) {
+					star += '*';
+				}
+			console.log(star)
+		}
+	} 
+
+	rysujChoinke(8)
+	
